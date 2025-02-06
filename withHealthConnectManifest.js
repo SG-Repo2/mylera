a788b91c3db0a57dfdffa0efe35e8cf797c8d0b7
SG-Repo2 <134559327+SG-Repo2@users.noreply.github.com>
2025-02-06 16:10:10 -0600

    refactor Health Connect plugin: move TypeScript file to dedicated directory and add setup documentation

--- withHealthConnectManifest.js
+++ withHealthConnectManifest.js
@@ -1,21 +1,17 @@
-import { ConfigPlugin, withAndroidManifest } from '@expo/config-plugins';
+const { withAndroidManifest } = require('@expo/config-plugins');
 
-const withHealthConnectManifest: ConfigPlugin = (config) => {
+const withHealthConnectManifest = (config) => {
   return withAndroidManifest(config, (config) => {
-    // Cast modResults to any to bypass type restrictions
-    const manifest = config.modResults as any;
+    const manifest = config.modResults;
     if (manifest && manifest.application && manifest.application[0]) {
-      // Locate the MainActivity
       const mainActivity = manifest.application[0].activity.find(
-        (activity: any) =>
+        (activity) =>
           activity['$'] && activity['$']['android:name'] === '.MainActivity'
       );
       if (mainActivity) {
-        // Ensure intent-filters exist
         if (!mainActivity['intent-filter']) {
           mainActivity['intent-filter'] = [];
         }
-        // Add the Health Connect permission rationale intent filter
         mainActivity['intent-filter'].push({
           action: [
             {
@@ -29,4 +25,4 @@ const withHealthConnectManifest: ConfigPlugin = (config) => {
   });
 };
 
-export default withHealthConnectManifest;
+module.exports = withHealthConnectManifest;
