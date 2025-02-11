5fd3252a53cc2e89c84469e663b5b2797f6c608b
SG-Repo2 <134559327+SG-Repo2@users.noreply.github.com>
2025-02-11 10:00:21 -0600

    removing uneeded documentation

--- src/services/__mocks__/supabaseClient.ts
+++
@@ -1,9 +0,0 @@
-export const supabase = {
-  from: jest.fn(() => ({
-    select: jest.fn().mockReturnThis(),
-    eq: jest.fn().mockReturnThis(),
-    order: jest.fn().mockReturnThis(),
-    limit: jest.fn().mockReturnThis(),
-    single: jest.fn().mockReturnThis(),
-  })),
-};
