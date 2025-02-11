1ef36e6b82e81651ff1459e2fe6e804ca74445ce
SG-Repo2 <134559327+SG-Repo2@users.noreply.github.com>
2025-02-11 15:29:50 -0600

    add intent filters for Health Connect and deep linking support

--- withHealthConnectManifest.js
+++ withHealthConnectManifest.js
@@ -12,12 +12,28 @@ const withHealthConnectManifest = (config) => {
         if (!mainActivity['intent-filter']) {
           mainActivity['intent-filter'] = [];
         }
+        // Add Health Connect intent filter
         mainActivity['intent-filter'].push({
           action: [
             {
               $: { 'android:name': 'androidx.health.ACTION_SHOW_PERMISSIONS_RATIONALE' },
             },
           ],
+        });
+
+        // Add deep linking intent filter for both schemes
+        mainActivity['intent-filter'].push({
+          action: [
+            { $: { 'android:name': 'android.intent.action.VIEW' } }
+          ],
+          category: [
+            { $: { 'android:name': 'android.intent.category.DEFAULT' } },
+            { $: { 'android:name': 'android.intent.category.BROWSABLE' } }
+          ],
+          data: [
+            { $: { 'android:scheme': 'mylera' } },
+            { $: { 'android:scheme': 'exp+mylera' } }
+          ]
         });
       }
     }
