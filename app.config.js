683348014bccc0f1e7bc272f96f297171459b355
SG-Repo2 <134559327+SG-Repo2@users.noreply.github.com>
2025-03-25 17:18:31 -0500

    Bump version to 1.0.7 in app.config.js and package.json

--- app.config.js
+++ app.config.js
@@ -10,7 +10,7 @@ module.exports = {
   expo: {
     name: "mylera",
     slug: "mylera",
-    version: "1.0.6",
+    version: "1.0.7",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "mylera",
